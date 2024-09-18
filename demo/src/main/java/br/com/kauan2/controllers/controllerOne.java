e10eafbfc2ebf493d0d6acefab2560629c2c8ea7
K4Muller <kauangalucio4@gmail.com>
2024-09-18 18:22:10 -0300

    Adicionando funcionalidades no controllerOne

--- demo/src/main/java/br/com/kauan2/controllers/controllerOne.java
+++ demo/src/main/java/br/com/kauan2/controllers/controllerOne.java
@@ -2,10 +2,13 @@ package br.com.kauan2.controllers;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +37,33 @@ public class controllerOne {
         return "metodoComBodyParams " + user.username();
     }
     record User(String username){
+
+    }
+
+    //Aula sobre header
+    @PostMapping("/java/metodoHeaderParams")
+    public String metodoHeader(@RequestHeader("name") String name) {
+        return "metodoComHeaderParams " + name;
+
+    }
+
+    //Usando lista no header
+    @PostMapping("/java/metodoListHeaderParams")
+    public String metodoListHeader(@RequestHeader Map<String, String> headers) {
+        return "metodoComHeaderParams " + headers.entrySet();
+    }
+
+    //Aula de Response Entity
+    @GetMapping("/java/responseEntity/{id}")
+    public ResponseEntity<Object> metodoResponseEntity(@PathVariable long id) {
+        var usuario = new User1("KAUAN");
+
+        if (id > 5) {
+            return ResponseEntity.status(HttpStatus.OK).body(usuario);
+        }
+        return ResponseEntity.badRequest().body("Numero menor que 5");
+    }
+    record User1(String username){
 
     }
 }
